8bb54a86e97326c4bc198d91b06bc398f4c48c94
Thouma <richard_allcca_llano@hotmail.com>
2025-02-28 15:54:06 -0500

    Mejorar comentarios en el archivo app.ts para mayor claridad en la inicialización del servidor y el servicio WebSocket

--- src/app.ts
+++ src/app.ts
@@ -4,25 +4,24 @@ import { AppRoutes } from './presentation/routes';
 import { Server } from './presentation/server';
 import { WssService } from './presentation/services/wss.service';
 
-
 (async()=> {
   main();
 })();
 
-
 function main() {
-
+  // Crear una instancia del servidor de la aplicación
   const server = new Server({
     port: envs.PORT,
     routes: AppRoutes.routes,
   });
 
+  // Crear un servidor HTTP utilizando la instancia del servidor de la aplicación
   const httpServer = createServer(server.app);
 
-  // Inicializar el servicio de WebSockets
+  // Inicializar el servicio WebSocket con el servidor HTTP
   WssService.initWss({ server: httpServer });
 
-  // server.start();
+  // Iniciar el servidor HTTP y escuchar en el puerto especificado
   httpServer.listen(envs.PORT, () => {
     console.log(`Server running on port ${ envs.PORT }`);
   });
